68e09f41944577ed9a900b5f58581ff1ce9d49d9
Tania Jacob <tanzjacob@gmail.com>
2016-09-27 13:05:35 +1300

    Add Muslim as a religion type

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,6 +1,6 @@
 class Attendee < ApplicationRecord
   validates :first_name, :surname, :address, :email, :fiance_full_name, :age, :phone_number, :religion, :wedding_date, :preferred_event, :post_wedding_address, presence: true
-  enum religion_type: [:ANGLICAN, :BUDDHIST, :CATHOLIC, :HINDU, :NON_RELIGIOUS, :PRESBYTERIAN]
+  enum religion_type: [:ANGLICAN, :BUDDHIST, :CATHOLIC, :HINDU, :MUSLIM, :NO_RELIGION, :PRESBYTERIAN]
 
   def full_name
     "#{first_name} #{surname}"
