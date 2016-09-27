d68762b04093119aaf6b85cd3b45165c615e4f4c
Tania Jacob <tanzjacob@gmail.com>
2016-09-27 13:22:51 +1300

    Restore previous change

--- app/assets/javascripts/faq.js
+++
@@ -1,9 +0,0 @@
-$('.question').click(function (e) {
-  e.preventDefault();
-  $('.answer').addClass('hidden');
-  var content_id = $(this).attr('class');
-  var numb = content_id.match(/\d/g).join("");
-  var total = '.a' + numb;
-  debugger
-  $(total).removeClass('hidden');
-});
