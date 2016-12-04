4862b1b560f63ab7370346216fa05596c9973983
Tania Jacob <tanzjacob@gmail.com>
2016-12-05 11:00:56 +1300

    Add new dates and cost

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-Event.create(event_date: "16-17 November 2016", event_name: "November 2016", location: "St Teresa's Parish, Karori")
-Event.create(event_date: "1-2 March 2017", event_name: "March 2017 (Date TBC)", location: "TBC")
-Event.create(event_date: "1-2 July 2017", event_name: "July 2017 (Date TBC)", location: "TBC")
-Event.create(event_date: "1-2 November 2017", event_name: "November 2017 (Date TBC)", location: "TBC")
+Event.create(event_date: "18-19 March 2017", event_name: "March 2017", location: "St Teresa's Catholic Parish, 301 Karori Road, Karori")
+Event.create(event_date: "22-23 July 2017", event_name: "July 2017", location: "St Teresa's Catholic Parish, 301 Karori Road, Karori")
+Event.create(event_date: "11-12 November 2017", event_name: "November 2017", location: "St Teresa's Catholic Parish, 301 Karori Road, Karori")
